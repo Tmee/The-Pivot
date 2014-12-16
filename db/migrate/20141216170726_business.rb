3796298aa3b4bbb1bbff4a5597a4a97ac00d3c8e
zRouth <zrouthier@gmail.com>
2014-12-16 10:46:16 -0700

    business migration added

--- db/migrate/20141216170726_business.rb
+++ db/migrate/20141216170726_business.rb
@@ -1,4 +1,9 @@
 class Business < ActiveRecord::Migration
   def change
+    create_table :business do |t|
+      t.string :name
+      t.string :address
+      t.text :description
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203220822) do
+ActiveRecord::Schema.define(version: 20141216170726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 20141203220822) do
   end
 
   add_index "addresses", ["order_id"], name: "index_addresses_on_order_id", using: :btree
+
+  create_table "business", force: true do |t|
+    t.string "name"
+    t.string "address"
+    t.text   "description"
+  end
 
   create_table "categories", force: true do |t|
     t.string   "title"
