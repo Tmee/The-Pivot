78c57f9311a3259005598efd2a7904dfe8f58247
Timothy Mee <tjmee90@gmail.com>
2014-12-16 10:50:16 -0700

    changed business to plural

--- app/controllers/business_controller.rb
+++ app/controllers/business_controller.rb
@@ -1,2 +1,35 @@
-class BusinessController < ApplicationController
+class BusinessesController < ApplicationController
+  before_action :current_business, only: [:show]
+  # before_action :require_admin, only: [:index]
+
+  def index
+    @business = Businesses.all
+  end
+
+  def new
+    @business = Businesses.new
+  end
+
+  def create
+    @business = Businesses.new(user_params)
+
+    if @business.save
+      session[:email] = @business.id
+      flash[:notice] = "Business created"
+      redirect_to root_path
+    else
+      flash[:notice] = "Business could not be created"
+      @errors = @user.errors.map do |attribute, msg|
+        "#{attribute.to_s.gsub("_", " ").capitalize}: #{msg.downcase}"
+      end.uniq
+      render :new
+    end
+  end
+
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :adress, :user_name, :password, :password_confirmation)
+  end
 end
--- db/migrate/20141216170726_business.rb
+++ db/migrate/20141216170726_business.rb
@@ -1,6 +1,6 @@
-class Business < ActiveRecord::Migration
+class Businesses < ActiveRecord::Migration
   def change
-    create_table :business do |t|
+    create_table :businesses do |t|
       t.string :name
       t.string :address
       t.text :description
