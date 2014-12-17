94b7398b75f622dd71bfd25b65827e01b15e22af
Timothy Mee <tjmee90@gmail.com>
2014-12-17 11:05:30 -0700

    can now create a listing, not sure if its linked to a business

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,5 @@
 class Listing < ActiveRecord::Base
   belongs_to :business
+
+  validates :title, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141217021403) do
+ActiveRecord::Schema.define(version: 20141217170455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,18 @@ ActiveRecord::Schema.define(version: 20141217021403) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "state"
+  end
+
+  create_table "listings", force: true do |t|
+    t.integer  "business_id"
+    t.integer  "wage"
+    t.integer  "hours"
+    t.integer  "number_of_positions"
+    t.string   "title"
+    t.string   "end_date"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
