cbfbdbc00b3bf29f9489eb5b6499e6db431894f7
zRouth <zrouthier@gmail.com>
2014-12-16 17:36:09 -0700

    added subdomain sort of kinda started Good luck

--- app/models/business.rb
+++ app/models/business.rb
@@ -2,10 +2,16 @@ class Business < ActiveRecord::Base
   has_many :users
 
   validates :name, presence: true
-  validates :address, presence: true
-  validates :phone, presence: true
-  validates :email, presence: true
-  validates :slug, presence: true
-  validates :url, presence: true
-  validates :description, presence: true
+  # validates :address, presence: true
+  # validates :phone, presence: true
+  # validates :email, presence: true
+  # validates :slug, presence: true
+  # validates :url, presence: true
+  # validates :description, presence: true
+
+  before_save :generate_slug
+
+  def generate_slug
+    self.slug = name.parameterize
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,46 @@
+class Subdomain
+  def self.matches?(request)
+    request.subdomain.present? && request.subdomain != "www" && request.subdomain != ""
+  end
+end
+
+# class RootDomain
+#   @subdomains = ["www"]
+
+#   def self.matches?(request)
+#     @subdomains.include?(request.subdomain) || request.subdomain.blank?
+#   end
+# end
+
+
 Rails.application.routes.draw do
+
+  # business-name.lvh.me
+  constraints(Subdomain) do
+    namespace :business do
+      resources :job_listings
+    end
+
+    resources :job_listings
+
+    get '/' => 'job_listings#show'
+  end
+
+
+
+
+  # resources :business, path: '', param: :slug
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  # namespace :businesses, as: :business, path: '/:slug' do
+  #   resources :job_listing
+  # end
+
+
   root 'home#index'
+
   get '/menu' => 'home#menu'
   get '/menu/:id' => 'home#yum'
   get '/contact' => 'home#contact'
@@ -31,3 +68,5 @@ Rails.application.routes.draw do
     resources :menu_items
   end
 end
+
+
