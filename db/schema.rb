9f1f0a9bc3c1cfb1a4ad3da7d4331c3ebf17f5e2
Timothy Mee <tjmee90@gmail.com>
2014-12-18 09:23:56 -0700

    fixed migration error

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141218020917) do
+ActiveRecord::Schema.define(version: 20141217170455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,13 +37,6 @@ ActiveRecord::Schema.define(version: 20141218020917) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "state"
-    t.string   "subdomain"
-  end
-
-  create_table "categories", force: true do |t|
-    t.string   "title"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "listings", force: true do |t|
