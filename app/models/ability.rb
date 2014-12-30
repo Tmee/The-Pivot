a2f29af74784cee5688b59e17c036f7bb3e4f418
Timothy Mee <tjmee90@gmail.com>
2014-12-30 12:04:04 -0700

    cleaned ability page

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,8 +6,7 @@ class Ability
     can :create, Business
     can :manage, Business, :id => user.business_id
     can :home, Business, :id => user.business_id
-    # raise "boom"
-    # can :manage, Business   if user == @business_owner
-    can :manage, User if user == @current_user
+
+    can :manage, User, :id => user.id
   end
 end
