4808084ee10c9e3926764cf9ee68b286029dda4e
Timothy Mee <tjmee90@gmail.com>
2015-01-05 10:38:53 -0700

    added .present? for require admin. used to break if current_user = nil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,7 @@ class ApplicationController < ActionController::Base
   end
 
   def require_admin
-    unless current_user && current_user.is_admin?
+    unless current_user.present? && current_user.is_admin?
       redirect_to root_path, :alert => "Not Authorized"
     end
   end
