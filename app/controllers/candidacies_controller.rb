9bc83e1794ba3bca2787d8df8c3e70c18e65b94f
zRouth <zrouthier@gmail.com>
2015-01-06 18:15:09 -0700

    ticket 92 and 22 bare bones completely working

--- app/controllers/candidacies_controller.rb
+++ app/controllers/candidacies_controller.rb
@@ -4,15 +4,35 @@ class CandidaciesController < ApplicationController
     @candidacy = Candidacy.new
   end
 
+  def index
+    @candidacies = Candidacy.where(listing_id: params[:id]).pending
+  end
+
   def create
     listing = Listing.find(params[:listing_id])
     listing.business_id
     @candidacy = Candidacy.create(listing_id: params[:listing_id],
-                                  user_id: current_user.id)
+                                  user_id: current_user.id,
+                                  letter_to_business: params[:candidacy][:letter_to_business])
 
 
     flash[:notice] = "Your application for #{listing.title} has been received and is pending."
     redirect_to cart_path
   end
+
+  def accept
+    candidacy = Candidacy.find_by_id(params[:id])
+    candidacy.accept
+    redirect_to :back
+  end
+
+  def reject
+    candidacy = Candidacy.find_by_id(params[:id])
+    candidacy.reject
+    redirect_to :back
+  end
+
+private
+
 
 end
--- app/models/candidacy.rb
+++ app/models/candidacy.rb
@@ -1,4 +1,21 @@
 class Candidacy < ActiveRecord::Base
   belongs_to :listing
   belongs_to :user
+
+  before_create :set_initial_status
+
+  scope :pending, -> { where(status: 'pending') }
+
+  def set_initial_status
+    self.status = "pending"
+  end
+
+  def reject
+    update_attribute :status, "rejected"
+  end
+
+  def accept
+    update_attribute :status, "accepted"
+  end
+
 end
