0047d08325433bfc2af10350bf452336b95f7ef7
zRouth <zrouthier@gmail.com>
2015-01-07 11:53:39 -0700

    added business and candidacy model spec tests, deleted whitespace in schema

--- spec/models/candidacy_spec.rb
+++ spec/models/candidacy_spec.rb
@@ -2,4 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Candidacy, :type => :model do
 
+  let(:candidacy) do
+    Candidacy.new(status: 'pending', letter_to_business: '')
+  end
+
+  it 'is valid without a status' do
+    candidacy.save
+    candidacy.status = nil
+    expect(candidacy).to be_valid
+  end
+
+  it 'is valid without a letter to business' do
+    candidacy.status = nil
+    expect(candidacy).to be_valid
+  end
+
 end
